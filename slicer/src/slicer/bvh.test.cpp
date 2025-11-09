c0c7d59983bb69a6992cdd3ca9973bb172f450b8
Daniel Toby <daniel.e.toby@gmail.com>
2025-11-09 12:34:23 -0500

    Added catch2 and slicer_tests.

---
+++ slicer/src/slicer/bvh.test.cpp
@@ -0,0 +1,13 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <ranges>
+
+#include "slicer/bvh.hpp"
+
+TEST_CASE("Views") {
+    auto vec = std::vector{1, 2, 3, 4, 5};
+    auto evens_view = vec | std::views::filter([](auto x) { return x % 2 == 0; });
+    auto evens = std::vector(evens_view.begin(), evens_view.end());
+
+    CHECK(evens == std::vector{2, 4});
+}
